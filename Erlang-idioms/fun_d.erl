2f81c870ad8b86f32cf1000a504192a4d5888728
Harry Daninthe <DDHarry@users.noreply.github.com>
2017-01-06 09:15:11 +0100

    Create fun_d.erl

---
+++ Erlang-idioms/fun_d.erl
@@ -0,0 +1,24 @@
+-module(fun_d).
+-export([dbl/1,mtpl/2,multiple/2,trpl/1]).
+
+%% Get the scheme
+dbl(X)  ->
+  Mult = fun(Times) -> (fun(Y) -> Times*Y end) end,
+  Double = Mult(2),
+  Double(X).
+
+%% A lighter one - see the anonymous variable X (l12)
+%% the compiler will emit a warning "X shadow variable"
+trpl(X) ->
+  Mult = fun(Times) -> (fun(X) -> Times*X end) end,
+  %%Trpl = Mult(3),
+ ( Mult(3) ) (X).
+
+%% Compacter - "Y, Times" are foo variable
+multiple(Tms,X) ->
+  Mult = fun(Times) -> (fun(Y) -> Times*Y end) end,
+ (Mult(Tms)) (X).
+
+%% Final version
+mtpl(Tms,X) ->
+  ((fun(Times) -> (fun(X) -> Times*X end) end)(Tms))(X).
